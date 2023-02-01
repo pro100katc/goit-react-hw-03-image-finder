70883f90264099a79a2101e2ca68f3492f3e4295
pro100katc <105969779+pro100katc@users.noreply.github.com>
2023-02-01 12:56:18 +0000

    Deploying to gh-pages from @ pro100katc/goit-react-hw-03-image-finder@4f569135bdef8cc4ccee6bed00b22df4ea5ed0f7 🚀

--- src/components/App.styled.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const AppContainer = styled.div`
-    display: grid;
-  grid-template-columns: 1fr;
-  grid-gap: 16px;
-  padding-bottom: 24px;
-`;
--- src/components/Loader/Loader.styled.jsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const LoaderStyled = styled.div`
-display: grid;
-  max-width: calc(100vw - 48px);
-  margin-left: auto;
-  margin-right: auto;
-`
